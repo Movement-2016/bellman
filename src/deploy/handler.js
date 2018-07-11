c6694f9e4a2d2823ab618342d1b29a0a7c9d0234
Alex Melman <melmanalex@gmail.com>
2018-07-11 18:27:23 -0500

    add js mime type

--- src/deploy/handler.js
+++ src/deploy/handler.js
@@ -1,25 +1,27 @@
-const AWS        = require('aws-sdk');
+const AWS = require('aws-sdk');
 const LambdaFunc = require('../lib/LambdaFunc');
-const path       = require('path');
+const path = require('path');
 
-const BUCKET_NAME         = 'movement.vote';
-const BUILD_PROJECT_NAME  = 'MovementVote';
+const BUCKET_NAME = 'movement.vote';
+const BUILD_PROJECT_NAME = 'MovementVote';
 const BUILD_PROJECT_REGEX = /^MovementVote/;
-const BUILD_BRANCH        = '';
-const REGION              = 'us-west-2';
+const BUILD_BRANCH = '';
+const REGION = 'us-west-2';
 
 class StartBuild extends LambdaFunc {
   perform() {
-    var codebuild = new AWS.CodeBuild({region: REGION});
+    var codebuild = new AWS.CodeBuild({ region: REGION });
 
     var params = {
-      projectName: BUILD_PROJECT_NAME, /* required */
-      sourceVersion: BUILD_BRANCH
+      projectName: BUILD_PROJECT_NAME /* required */,
+      sourceVersion: BUILD_BRANCH,
     };
 
-    codebuild.startBuild(params).promise()
-      .then( this.thenHandler )
-      .catch( this.errorHandler );
+    codebuild
+      .startBuild(params)
+      .promise()
+      .then(this.thenHandler)
+      .catch(this.errorHandler);
   }
 }
 
@@ -30,58 +32,60 @@ class Deploy extends LambdaFunc {
     let newBuildFiles;
     let oldBuildFiles;
 
-    s3.listObjects({Bucket:BUCKET_NAME}).promise().then( d => {
+    s3.listObjects({ Bucket: BUCKET_NAME })
+      .promise()
+      .then(d => {
+        const fileNames = d.Contents.map(c => c.Key);
+        newBuildFiles = fileNames.filter(f => BUILD_PROJECT_REGEX.test(f));
+        oldBuildFiles = fileNames.filter(f => !BUILD_PROJECT_REGEX.test(f));
 
-      const fileNames = d.Contents.map( c => c.Key );
-      newBuildFiles = fileNames.filter( f => BUILD_PROJECT_REGEX.test(f) );
-      oldBuildFiles = fileNames.filter( f => !BUILD_PROJECT_REGEX.test(f) );
+        const dparams = {
+          Bucket: BUCKET_NAME,
+          Delete: {
+            Objects: oldBuildFiles.map(Key => ({ Key })),
+          },
+        };
 
-      const dparams = {
-        Bucket: BUCKET_NAME,
-        Delete: {
-          Objects: oldBuildFiles.map( Key => ({ Key }) )
-        },
-      };
+        return oldBuildFiles.length
+          ? s3.deleteObjects(dparams).promise()
+          : { msg: 'no files to delete' };
+      })
+      .then(() => {
+        const params = newBuildFiles.map(_calcParams);
 
-      return oldBuildFiles.length ? s3.deleteObjects( dparams ).promise() : { msg: 'no files to delete'};
-
-    }).then( () => {
-
-      const params = newBuildFiles.map( _calcParams );
-
-      return Promise.all( params.map( copyPromise(s3) ) )
-                     .then( results => results.map( ({CopyObjectResult}) => CopyObjectResult ) );
-    })
-    .then( this.thenHandler )
-    .catch( this.errorHandler );
-
+        return Promise.all(params.map(copyPromise(s3))).then(results =>
+          results.map(({ CopyObjectResult }) => CopyObjectResult)
+        );
+      })
+      .then(this.thenHandler)
+      .catch(this.errorHandler);
   }
 }
 
-const copyPromise = s3 => p => s3.copyObject( p ).promise();
+const copyPromise = s3 => p => s3.copyObject(p).promise();
 
 const map = {
-  '.svg':  'image/svg+xml',
+  '.svg': 'image/svg+xml',
   '.html': 'text/html',
-  '.htm':  'text/html',
-  '.png':  'image/png',
-  '.jpg':  'image/jpeg',
-  '.css':  'text/css'
+  '.htm': 'text/html',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.css': 'text/css',
+  '.js': 'text/javascript',
 };
 
 const _calcParams = key => {
-
   const args = {
-          CopySource:          BUCKET_NAME + '/' + key,
-          Key:                 key.replace(BUILD_PROJECT_NAME + '/dist/public/', ''),
-          Bucket:              BUCKET_NAME,
-          ACL:                 'public-read',
-          ServerSideEncryption: null
-        };
+    CopySource: BUCKET_NAME + '/' + key,
+    Key: key.replace(BUILD_PROJECT_NAME + '/dist/public/', ''),
+    Bucket: BUCKET_NAME,
+    ACL: 'public-read',
+    ServerSideEncryption: null,
+  };
 
   const ext = path.parse(key).ext;
 
-  if( map[ext] ) {
+  if (map[ext]) {
     args.ContentType = map[ext];
     args.Metadata = {};
     args.MetadataDirective = 'REPLACE';
@@ -90,8 +94,7 @@ const _calcParams = key => {
   return args;
 };
 
-
 module.exports = {
-  build: (new StartBuild()).handler,
-  deploy: (new Deploy()).handler
+  build: new StartBuild().handler,
+  deploy: new Deploy().handler,
 };
