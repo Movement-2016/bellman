05f3e1a574622d332a3376e6c3dbf7fc9cefa935
Alex Melman <melmanalex@gmail.com>
2018-07-11 15:30:20 -0500

    new admin email

--- clients/index.js
+++ clients/index.js
@@ -2,22 +2,22 @@
 /* auto generated */ 
 var a = require('./lib/Service');
 
-var b = require('./email');
+var b = require('./deploy');
 
-var c = require('./deploy');
+var c = require('./email');
 
-var d = require('./email');
+var d = require('./deploy');
 
-var e = require('./deploy');
+var e = require('./email');
 
 module.exports = {
    prod: {
-   email:  cfg => new b({...cfg,endpoint:'https://9gvdc17imc.execute-api.us-west-2.amazonaws.com/prod',slug:'email'}),
+   deploy:  cfg => new b({...cfg,endpoint:'https://f86r0juvaa.execute-api.us-west-2.amazonaws.com/prod',slug:'deploy'}),
 plans:  cfg => new a({...cfg,endpoint:'https://l7y8vcdf01.execute-api.us-west-2.amazonaws.com/prod',slug:'plans'}),
-deploy:  cfg => new c({...cfg,endpoint:'https://f86r0juvaa.execute-api.us-west-2.amazonaws.com/prod',slug:'deploy'}),
+email:  cfg => new c({...cfg,endpoint:'https://9gvdc17imc.execute-api.us-west-2.amazonaws.com/prod',slug:'email'}),
 users:  cfg => new a({...cfg,endpoint:'https://xds1htuk84.execute-api.us-west-2.amazonaws.com/prod',slug:'users'})},
 dev: {
-   email:  cfg => new d({...cfg,endpoint:'https://wmg82tv0ka.execute-api.us-west-2.amazonaws.com/dev',slug:'email'}),
-deploy:  cfg => new e({...cfg,endpoint:'https://qf7gz2uqq2.execute-api.us-west-2.amazonaws.com/dev',slug:'deploy'}),
+   deploy:  cfg => new d({...cfg,endpoint:'https://qf7gz2uqq2.execute-api.us-west-2.amazonaws.com/dev',slug:'deploy'}),
 plans:  cfg => new a({...cfg,endpoint:'https://nof8onrfef.execute-api.us-west-2.amazonaws.com/dev',slug:'plans'}),
+email:  cfg => new e({...cfg,endpoint:'https://wmg82tv0ka.execute-api.us-west-2.amazonaws.com/dev',slug:'email'}),
 users:  cfg => new a({...cfg,endpoint:'https://3wim9e9xf9.execute-api.us-west-2.amazonaws.com/dev',slug:'users'})}};
--- dist/index.js
+++ dist/index.js
@@ -5,37 +5,37 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
 /* auto generated */
 var a = require('./lib/Service');
 
-var b = require('./email');
+var b = require('./deploy');
 
-var c = require('./deploy');
+var c = require('./email');
 
-var d = require('./email');
+var d = require('./deploy');
 
-var e = require('./deploy');
+var e = require('./email');
 
 module.exports = {
    prod: {
-      email: function email(cfg) {
-         return new b(_extends({}, cfg, { endpoint: 'https://9gvdc17imc.execute-api.us-west-2.amazonaws.com/prod', slug: 'email' }));
+      deploy: function deploy(cfg) {
+         return new b(_extends({}, cfg, { endpoint: 'https://f86r0juvaa.execute-api.us-west-2.amazonaws.com/prod', slug: 'deploy' }));
       },
       plans: function plans(cfg) {
          return new a(_extends({}, cfg, { endpoint: 'https://l7y8vcdf01.execute-api.us-west-2.amazonaws.com/prod', slug: 'plans' }));
       },
-      deploy: function deploy(cfg) {
-         return new c(_extends({}, cfg, { endpoint: 'https://f86r0juvaa.execute-api.us-west-2.amazonaws.com/prod', slug: 'deploy' }));
+      email: function email(cfg) {
+         return new c(_extends({}, cfg, { endpoint: 'https://9gvdc17imc.execute-api.us-west-2.amazonaws.com/prod', slug: 'email' }));
       },
       users: function users(cfg) {
          return new a(_extends({}, cfg, { endpoint: 'https://xds1htuk84.execute-api.us-west-2.amazonaws.com/prod', slug: 'users' }));
       } },
    dev: {
-      email: function email(cfg) {
-         return new d(_extends({}, cfg, { endpoint: 'https://wmg82tv0ka.execute-api.us-west-2.amazonaws.com/dev', slug: 'email' }));
-      },
       deploy: function deploy(cfg) {
-         return new e(_extends({}, cfg, { endpoint: 'https://qf7gz2uqq2.execute-api.us-west-2.amazonaws.com/dev', slug: 'deploy' }));
+         return new d(_extends({}, cfg, { endpoint: 'https://qf7gz2uqq2.execute-api.us-west-2.amazonaws.com/dev', slug: 'deploy' }));
       },
       plans: function plans(cfg) {
          return new a(_extends({}, cfg, { endpoint: 'https://nof8onrfef.execute-api.us-west-2.amazonaws.com/dev', slug: 'plans' }));
+      },
+      email: function email(cfg) {
+         return new e(_extends({}, cfg, { endpoint: 'https://wmg82tv0ka.execute-api.us-west-2.amazonaws.com/dev', slug: 'email' }));
       },
       users: function users(cfg) {
          return new a(_extends({}, cfg, { endpoint: 'https://3wim9e9xf9.execute-api.us-west-2.amazonaws.com/dev', slug: 'users' }));
